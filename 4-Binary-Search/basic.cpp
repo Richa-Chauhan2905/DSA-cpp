dff63871c17f1d609b10b760e820236e78065303
Richa-Chauhan2905 <chauhanricha2911@gmail.com>
2025-08-17 08:01:05 +0530

    first and last occurence

--- 4-Binary-Search/basic.cpp
+++ 4-Binary-Search/basic.cpp
@@ -2,58 +2,71 @@
 
 using namespace std;
 
-int binarySearch(vector<int> &arr, int target){
+int binarySearch(vector<int> &arr, int target)
+{
     int n = arr.size();
     int low = 0;
-    int high = n-1;
+    int high = n - 1;
 
     while (low <= high)
     {
-        int mid = (low + high)/2;
-        if(arr[mid] == target) return mid;
-        else if(arr[mid] > target){
+        int mid = (low + high) / 2;
+        if (arr[mid] == target)
+            return mid;
+        else if (arr[mid] > target)
+        {
             high = mid - 1;
         }
-        else{
+        else
+        {
             low = mid + 1;
         }
     }
     return -1;
 }
 
-int main(){
+int main()
+{
     vector<int> arr = {1, 2, 4, 6, 8, 10, 14};
     int result = binarySearch(arr, 1);
     cout << "Target found at index: " << result;
 }
 
-//lower bound(stl version)
-int main() {
+// lower bound(stl version)
+int main()
+{
     vector<int> arr = {1, 2, 4, 4, 5, 7, 9};
     int x = 4;
     auto it = lower_bound(arr.begin(), arr.end(), x);
     cout << "Index: " << (it - arr.begin()) << endl;
 }
 
-//lower bound
-int lowerBound(vector<int>& arr, int x) {
+// lower bound
+int lowerBound(vector<int> &arr, int x)
+{
     int n = arr.size();
-    int low = 0, high = n-1; 
+    int low = 0, high = n - 1;
     int ans = n;
-    
-    while (low <= high) {
+
+    while (low <= high)
+    {
         int mid = (low + high) / 2;
 
-        if (arr[mid] <= x) {
-            low = mid + 1;  
-        } else {
-            high = mid - 1;    
+        if (arr[mid] >= x)
+        {
+            ans = mid;
+            high = mid - 1;
+        }
+        else
+        {
+            low = mid + 1;
         }
     }
-    return low;
+    return ans;
 }
 
-int main() {
+int main()
+{
     vector<int> arr = {1, 2, 4, 4, 5, 7, 9};
     int x;
 
@@ -63,7 +76,7 @@ int main() {
     int index = lowerBound(arr, x);
 
     if (index < arr.size())
-        cout << "Lower bound of " << x << " is at index " << index 
+        cout << "Lower bound of " << x << " is at index " << index
              << " with value " << arr[index] << endl;
     else
         cout << "Lower bound of " << x << " is beyond the array (index = " << index << ")" << endl;
@@ -71,25 +84,32 @@ int main() {
     return 0;
 }
 
-//upper bound
-int upperBound(vector<int>& arr, int x) {
+// upper bound
+int upperBound(vector<int> &arr, int x)
+{
     int n = arr.size();
-    int low = 0, high = n-1; 
+    int low = 0, high = n - 1;
     int ans = n;
-    
-    while (low <= high) {
+
+    while (low <= high)
+    {
         int mid = (low + high) / 2;
 
-        if (arr[mid] < x) {
-            low = mid + 1;  
-        } else {
-            high = mid - 1;    
+        if (arr[mid] > x)
+        {
+            ans = mid;
+            high = mid - 1;
+        }
+        else
+        {
+            low = mid + 1;
         }
     }
-    return low;
+    return ans;
 }
 
-int main() {
+int main()
+{
     vector<int> arr = {1, 2, 4, 4, 5, 7, 9};
     int x;
 
@@ -99,10 +119,20 @@ int main() {
     int index = upperBound(arr, x);
 
     if (index < arr.size())
-        cout << "Upper bound of " << x << " is at index " << index 
+        cout << "Upper bound of " << x << " is at index " << index
              << " with value " << arr[index] << endl;
     else
         cout << "Upper bound of " << x << " is beyond the array (index = " << index << ")" << endl;
 
     return 0;
+}
+
+// first and last occurrence
+vector<int> searchRange(vector<int> &nums, int target)
+{
+    int n = nums.size();
+    int lb = lowerBound(nums, target);
+    if (lb == n || nums[lb] != target)
+        return {-1, -1};
+    return {lb, upperBound(nums, target) - 1};
 }
